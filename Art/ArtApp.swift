32585187b05aa07a5eae717126a8375503092935
Chinmay Bansal <chinmaybansalsc@outlook.com>
2022-08-02 11:39:47 -0700

    Initial Commit

---
+++ Art/ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtApp.swift
+//  Art
+//
+//  Created by Chinmay Bansal on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
